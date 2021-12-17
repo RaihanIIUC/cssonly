52a42816bd202c1c1c0e71b30699d90ab914d719
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2021-12-17 09:15:30 +0600

    done by self

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <div className="header">Header</div>
-        <div className="content-large">Content #1</div>
-        <div className="content-small">content #2</div>
-        <div className="content-small">content #3</div>
-        <div className="footer">Content #4</div>
-      </div>
+    <div className="container">
+      <div className="header">#header</div>
+      <div className="content-large">#content 2</div>
+      <div className="content-sidebar">#content 3</div>
+      <div className="footer">Footer</div>
     </div>
   );
 }
