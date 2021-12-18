bbd2ee3b86bbca8d57338a10661e9e14813314df
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2021-12-18 12:41:15 +0600

    kevin powel

--- src/App.js
+++ src/App.js
@@ -2,11 +2,120 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="maindiv">
-      <div className="div1">Box 1</div>
-      <div className="div2">Box 2</div>
-      <div className="div3">Box 3</div>
-      <div className="div4">Box 4</div>
+    <div className="container">
+      <div className="heading">
+        <h1>Css Responsive flex cards </h1>
+      </div>
+
+      <div className="row">
+        <div className="card">
+          <div className="card-header">
+            <h1>React</h1>
+          </div>
+
+          <div className="card-body">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            </p>
+            <a href="" className="btn">
+              Read More...
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header">
+            <h1>Vue</h1>
+          </div>
+
+          <div className="card-body">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            </p>
+            <a href="" className="btn">
+              Read More...
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header">
+            <h1>Angular</h1>
+          </div>
+
+          <div className="card-body">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            </p>
+            <a href="" className="btn">
+              Read More...
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header">
+            <h1>Sass</h1>
+          </div>
+
+          <div className="card-body">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nobis
+              eaque aperiam eius deleniti odio eligendi inventore dolor nulla
+              perferendis odit quae, non beatae aspernatur tempore modi, vitae
+              doloribus enim deserunt dignissimos accusantium! Maxime ipsum quis
+              fugit dolores temporibus molestias laborum, quos, ad vel non
+              necessitatibus cupiditate officia vitae sit.
+            </p>
+            <a href="" className="btn">
+              Read More...
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header">
+            <h1>Sass</h1>
+          </div>
+
+          <div className="card-body">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nobis
+              eaque aperiam eius deleniti odio eligendi inventore dolor nulla
+              perferendis odit quae, non beatae aspernatur tempore modi, vitae
+              doloribus enim deserunt dignissimos accusantium! Maxime ipsum quis
+              fugit dolores temporibus molestias laborum, quos, ad vel non
+              necessitatibus cupiditate officia vitae sit.
+            </p>
+            <a href="" className="btn">
+              Read More...
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header">
+            <h1>Sass</h1>
+          </div>
+
+          <div className="card-body">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nobis
+              eaque aperiam eius deleniti odio eligendi inventore dolor nulla
+              perferendis odit quae, non beatae aspernatur tempore modi, vitae
+              doloribus enim deserunt dignissimos accusantium! Maxime ipsum quis
+              fugit dolores temporibus molestias laborum, quos, ad vel non
+              necessitatibus cupiditate officia vitae sit.
+            </p>
+            <a href="" className="btn">
+              Read More...
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
